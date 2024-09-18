c5bcf6af37204ec09820444f137609faf3407121
KingZCoding <traenknercody@gmail.com>
2024-09-18 17:18:52 -0400

    almost done js

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ const operators = document.querySelectorAll('.operator');
 const digits = document.querySelectorAll('.digit');
 const extras = document.querySelectorAll('.extras');
 const screen = document.getElementById('screen');
+const equal = document.getElementById('equal');
+const clear = document.getElementById('clear');
+const del = document.getElementById('del');
 
 let firstOperand = '';
 let secondOperand = '';
 let currentOperator = null;
+let shouldClearScreen = false;
 
 function selectDigit(e) {
+  if (shouldClearScreen) {
+    screen.textContent = '';
+    shouldClearScreen = false;
+  }
   let currentDigit = e.target.textContent;
   if (currentOperator === null) {
     firstOperand += currentDigit;
     console.log(firstOperand);
   } else {
     secondOperand += currentDigit;
+    console.log(secondOperand);
   }
 
   if (screen.textContent === '0') {
@@ -25,21 +34,73 @@ function selectDigit(e) {
 
 function selectOperator(e) {
   let selectedOperator = e.target.textContent;
-  if (currentOperator === null) {
-    currentOperator = selectedOperator;
-  } else {
-    return;
+  if (currentOperator !== null && secondOperand !== '') {
+    operate();
   }
+  currentOperator = selectedOperator;
+  shouldClearScreen = true;
 
-  if (currentOperator === selectedOperator) {
-    screen.textContent = `${currentdigit} + ${selectedOperator}`;
-  }
   console.log(selectedOperator);
 }
 
-function selectExtras(e) {
-  console.log(e.target.textContent);
+function add() {
+  return parseFloat(firstOperand) + parseFloat(secondOperand);
 }
+
+function subtract() {
+  return parseFloat(firstOperand) - parseFloat(secondOperand);
+}
+
+function multiply() {
+  return parseFloat(firstOperand) * parseFloat(secondOperand);
+}
+
+function divide() {
+  return parseFloat(firstOperand) / parseFloat(secondOperand);
+}
+
+function operate() {
+  let result = 0;
+  if (currentOperator === '+') {
+    result = add();
+  } else if (currentOperator === '-') {
+    result = subtract();
+  } else if (currentOperator === '*') {
+    result = multiply();
+  } else if (currentOperator === '/') {
+    result = divide();
+  }
+  screen.textContent = result;
+
+  firstOperand = result.toString();
+  secondOperand = '';
+  currentOperator = null;
+  shouldClearScreen = true;
+}
+
+function clearResult() {
+  firstOperand = '';
+  secondOperand = '';
+  currentOperator = null;
+  shouldClearScreen = true;
+  screen.textContent = '0';
+}
+
+function delLastNum() {
+  if (currentOperator === null) {
+    firstOperand = firstOperand.length > 1 ? firstOperand.slice(0, -1) : '0';
+    screen.textContent = firstOperand;
+  } else {
+    secondOperand = secondOperand.length > 1 ? secondOperand.slice(0, -1) : '0';
+    screen.textContent = secondOperand;
+  }
+}
+
+equal.addEventListener('click', operate);
+
+clear.addEventListener('click', clearResult);
+
+del.addEventListener('click', delLastNum);
 
 operators.forEach(operator => {
   operator.addEventListener('click', selectOperator);
@@ -47,8 +108,4 @@ operators.forEach(operator => {
 
 digits.forEach(digit => {
   digit.addEventListener('click', selectDigit);
-});
-
-extras.forEach(extra => {
-  extra.addEventListener('click', selectExtras);
 });
